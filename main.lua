de0f5dedc382926bff7dc740d5c67f5a3d23cbfc
JS1B <p.krzeminski7@gmail.com>
2023-05-27 18:25:11 +0200

    Add wsad speed control for target

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local Target = require "target"
 local Missile = require "missile"
+local suit = require "SUIT"
 
 -- Load function in the LÖVE framework
 function love.load()
@@ -7,16 +8,15 @@ function love.load()
     love.window.setTitle("Rocket simulator")
     -- Create a new instance of the MovingTarget class
     mytarget = Target:new(10, 50, 25)
-    mymissile = Missile:new(20, love.graphics.getHeight()-10, 50)
+    mymissile = Missile:new(20, love.graphics.getHeight()-10, 80)
 end
 
 -- Update function in the LÖVE framework
-
 local lastTime = 0.0
 function love.update(dt)
     -- Update the target's position
     mytarget:update(dt)
-    mymissile:update(dt, mytarget, "PG")
+    mymissile:update(dt, mytarget, "PG") -- PN PG
 
     -- Trigger points/dots draw
     local curTime = love.timer.getTime()
@@ -34,7 +34,16 @@ function love.draw()
 end
 
 function love.keypressed(key)
+    local speed = 10
     if key == "escape" then
         love.event.quit()
+    elseif key == "w" then
+        mytarget.velocity.y = mytarget.velocity.y - speed
+    elseif key == "s" then
+        mytarget.velocity.y = mytarget.velocity.y + speed
+    elseif key == "a" then
+        mytarget.velocity.x = mytarget.velocity.x - speed
+    elseif key == "d" then
+        mytarget.velocity.x = mytarget.velocity.x + speed
     end
 end
