2818c33f4e5d911dc0a1b58f55db4291fc15f8db
Piotr Krzemiński <p.krzeminski7@gmail.com>
2023-12-25 11:28:58 +0100

    Fix aliases craetion

--- setup.sh
+++ setup.sh
@@ -3,6 +3,20 @@
 
 echo "Setting up rocket-simulator..."
 
+# Function to create alias if it doesn't exist
+create_alias() {
+  local name=$1
+  local command=$2
+
+  # Check if alias is already set in the current session
+  if alias "$name" &>/dev/null; then
+    echo "Alias for $name already exists in the current session."
+  else
+    alias "$name=$command"
+    echo "Alias for $name created for the current session."
+  fi
+}
+
 # Check if required programs are installed
 for cmd in git cmake docker; do
   if ! command -v $cmd &> /dev/null; then
@@ -21,12 +35,7 @@ cmake -B build lib/love && cmake --build build || { echo "Error: Failed to build
 docker build -t ghcr.io/lunarmodules/busted:latest lib/busted || { echo "Error: Failed to build busted." >&2; return 1; }
 
 # Create aliases
-if [ -z "$(alias busted)" ]; then
-  alias busted='docker run -v "$(pwd):/data" ghcr.io/lunarmodules/busted:latest'
-fi
-
-if [ -z "$(alias love)" ]; then
-  alias love='build/love'
-fi
+create_alias busted 'docker run -v "$(pwd):/data" ghcr.io/lunarmodules/busted:latest'
+create_alias love 'build/love'
 
 echo "Setup completed successfully!"
